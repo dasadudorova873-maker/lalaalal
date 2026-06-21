c0bf47004fcac256a3e6c9cf34572f68e2045dfa
dasadudorova873-maker <dasadudorova873@gmail.com>
2026-06-22 01:53:07 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+'use strict'
+document.addEventListener("DOMContentLoaded", function(){
+    console.log('Скрипт сработал корректно')
+});
+const welcomeButtonModal = document.querySelector(".button");
+const modalApplication=document.querySelector(".applications");
+if (welcomeButtonModal && modalApplication){
+    welcomeButtonModal.addEventListener("click", function(event){
+        modalApplication.removeAttribute("hidden");
+    });
+}
+window.addEventListener("click", function(event){
+    if (event.target === modalApplication){
+        modalApplication.setAttribute("hidden", true);
+    }
+});
+const closeModalButton=document.querySelector(".application_close");
+closeModalButton.addEventListener("click", function(event){
+    modalApplication.setAttribute("hidden", true);
+});
+const faqQuestions=document.querySelectorAll('.question');
+faqQuestions.forEach(function(item){
+    item.addEventListener('click', function(){
+        item.classList.toggle('active');
+    });
+});
