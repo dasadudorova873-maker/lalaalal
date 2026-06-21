29b9fe6838b9fc6444a5f5f367fc19dd4bb2f82e
dasadudorova873-maker <dasadudorova873@gmail.com>
2026-06-22 01:59:11 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,16 @@
-'use strict'
-document.addEventListener("DOMContentLoaded", function(){
-    console.log('Скрипт сработал корректно')
-});
-const welcomeButtonModal = document.querySelector(".button");
-const modalApplication=document.querySelector(".applications");
-if (welcomeButtonModal && modalApplication){
-    welcomeButtonModal.addEventListener("click", function(event){
-        modalApplication.removeAttribute("hidden");
-    });
-}
-window.addEventListener("click", function(event){
-    if (event.target === modalApplication){
-        modalApplication.setAttribute("hidden", true);
+// FAQ accordion logic
+
+const questions = document.querySelectorAll(".faq-q");
+
+questions.forEach(btn => {
+  btn.addEventListener("click", () => {
+    const answer = btn.nextElementSibling;
+
+    // toggle
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
     }
-});
-const closeModalButton=document.querySelector(".application_close");
-closeModalButton.addEventListener("click", function(event){
-    modalApplication.setAttribute("hidden", true);
-});
-const faqQuestions=document.querySelectorAll('.question');
-faqQuestions.forEach(function(item){
-    item.addEventListener('click', function(){
-        item.classList.toggle('active');
-    });
+  });
 });
